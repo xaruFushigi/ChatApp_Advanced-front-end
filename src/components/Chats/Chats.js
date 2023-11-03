aa4d25a4e58d4ce9546d91946422a1226629edb8
xaruFushigi <xaru.fushigi@gmail.com>
2023-11-03 20:29:10 +0900

    Update Chats.js

--- src/components/Chats/Chats.js
+++ src/components/Chats/Chats.js
@@ -100,7 +100,7 @@ const Chats = () => {
       <h1>Chats</h1>
       <ChatEngine
         height="calc(80vh)"
-        projectID={projectId}
+        projectID="73ed9d82-e726-43ed-976c-7268f1d5e92a"
         userName={user.email}
         userSecret={user.uid}
       />
